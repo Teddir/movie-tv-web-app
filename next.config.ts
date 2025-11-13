51492b68336c02261fcb69b7693c0c88db21a25a
Teddir <ferryaja20000@gmail.com>
2025-11-13 20:11:25 +0700

    fix: update next config (settup env)

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    TMDB_ACCESS_TOKEN: process.env.TMDB_ACCESS_TOKEN,
+    TMDB_API_KEY: process.env.TMDB_API_KEY,
+  }
 };
 
 export default nextConfig;
